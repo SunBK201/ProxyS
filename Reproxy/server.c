9dad40972f706b066f2e178be948c47a4b6ec9c9
SunBK201 <1178117938@qq.com>
2020-07-26 20:59:32 +0800

    fix some bug

--- Reproxy/server.c
+++ Reproxy/server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,10 +10,15 @@
 #include <netdb.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "util.h"
+
+#define DEBUG
 
 #define SERV_PORT 23333
 #define REMOTE_IP "210.44.144.3"
+//#define REMOTE_IP "172.25.64.1"
+//#define REMOTE_IP "47.94.42.154"
 #define REMOTE_PORT 80
 
 #define MAX_BUFFER 8192
@@ -26,14 +32,10 @@ int remote_port;
 char client_host[128];
 int client_port;
 
-char *client_buffer;
-
 int connect_remote();
 int creat_server_socket(int port);
-int extract_host(const char *header);
 void forward_data(int source_socket, int destination_socket);
-void handle_client(int client_socket, struct sockaddr_in client_addr);
-int read_from_client(int client_socket);
+void handle_client(struct sockaddr_in client_addr);
 void server_deal();
 void sigchld_handler(int signal);
 
@@ -43,6 +45,10 @@ int creat_server_socket(int port)
     struct sockaddr_in server_addr;
 
     server_socket = Socket(AF_INET, SOCK_STREAM, 0);
+
+#ifdef DEBUG
+    printf("creat server socket\n");
+#endif
 
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
@@ -55,62 +61,23 @@ int creat_server_socket(int port)
 
     Listen(server_socket, 128);
 
-    printf("creat_server_socket\n");
-
     return server_socket;
 }
 
-int extract_host(const char *header)
+void setnonblocking(int sock)
 {
-    char *p = strstr(header, "Host:");
-
-    char *p2 = strchr(p + 5, ':');
-
-    char *p1 = strchr(p, '\n');
-
-    if (p2 && p2 < p1)
+    int opts;
+    opts = fcntl(sock, F_GETFL);
+    if (opts < 0)
     {
-
-        int p_len = (int)(p1 - p2 - 1);
-        char s_port[p_len];
-        strncpy(s_port, p2 + 1, p_len);
-        s_port[p_len] = '\0';
-        remote_port = atoi(s_port);
-
-        int h_len = (int)(p2 - p - 5 - 1);
-        strncpy(remote_host, p + 5 + 1, h_len);
-        remote_host[h_len] = '\0';
+        perror("fcntl(sock,GETFL)");
+        exit(1);
     }
-    else
+    opts = opts | O_NONBLOCK;
+    if (fcntl(sock, F_SETFL, opts) < 0)
     {
-        int h_len = (int)(p1 - p - 5 - 1 - 1);
-        strncpy(remote_host, p + 5 + 1, h_len);
-        remote_host[h_len] = '\0';
-        remote_port = 80;
-    }
-    return 0;
-}
-
-int read_from_client(int client_socket)
-{
-    memset(client_buffer, 0, MAX_BUFFER);
-    char line_buffer[2048];
-    char *base_ptr = client_buffer;
-
-    while (1)
-    {
-        int total_read = Readline(client_socket, line_buffer, 2048);
-
-        if (base_ptr + total_read - client_buffer <= MAX_BUFFER)
-        {
-            strncpy(base_ptr, line_buffer, total_read);
-            base_ptr += total_read;
-        }
-
-        if (strcmp(line_buffer, "\r\n") == 0 || strcmp(line_buffer, "\n") == 0)
-        {
-            break;
-        }
+        perror("fcntl(sock,SETFL,opts)");
+        exit(1);
     }
 }
 
@@ -138,66 +105,53 @@ void forward_data(int source_socket, int destination_socket)
 
     while ((n = recv(source_socket, buffer, MAX_BUFFER, 0)) > 0)
     {
-        if(source_socket == remote_socket)
+#ifdef DEBUG
+        if (source_socket == remote_socket)
         {
-            printf("remote[%s:%d] data to server and send to client[%s:%d]\n", remote_host, remote_port, client_host, client_port);
+            printf("remote[%s:%d] send to client[%s:%d], size=%d\n", REMOTE_IP, REMOTE_PORT, client_host, client_port, n);
         }
-        if(source_socket == client_socket)
+        if (source_socket == client_socket)
         {
-            printf("client[%s:%d] data to server and send to remote[%s:%d]\n", client_host, client_port, remote_host, remote_port);
+            printf("--------------------------------------------------------------\n");
+            printf("client[%s:%d] send to remote[%s:%d], size=%d, client request:\n%s\n", client_host, client_port, REMOTE_IP, REMOTE_PORT, n, buffer);
+            printf("--------------------------------------------------------------\n");
         }
+#endif
         send(destination_socket, buffer, n, 0);
     }
 
     shutdown(destination_socket, SHUT_RDWR);
-
     shutdown(source_socket, SHUT_RDWR);
 }
 
-void handle_client(int client_socket, struct sockaddr_in client_addr)
+void handle_client(struct sockaddr_in client_addr)
 {
-    read_from_client(client_socket);
-
-    //extract_host(client_buffer);
 
     remote_socket = connect_remote();
 
     if (fork() == 0)
     {
-
-        if (strlen(client_buffer) > 0)
-        {
-            /*
-            char *p = strstr(client_buffer, "CONNECT");
-            printf("%s\n", client_buffer);
-            if (p)
-            {
-                str_rep(client_buffer, client_buffer, "CONNECT", "GET");
-                printf("%s\n", client_buffer);
-            }
-            */
-
-            printf("send header to %s from client %s:%d\n", REMOTE_IP, client_host, client_port);
-            send(remote_socket, client_buffer, strlen(client_buffer), 0);
-        }
-        
         forward_data(client_socket, remote_socket);
+        Close(client_socket);
+        Close(remote_socket);
         exit(0);
     }
 
     if (fork() == 0)
     {
-
         forward_data(remote_socket, client_socket);
+        Close(remote_socket);
+        Close(client_socket);
         exit(0);
     }
-
     Close(remote_socket);
     Close(client_socket);
 }
 
 void server_deal()
 {
+    printf("server start listen...\n");
+
     struct sockaddr_in client_addr;
     socklen_t addrlen = sizeof(client_addr);
     char str[INET_ADDRSTRLEN];
@@ -207,17 +161,18 @@ void server_deal()
 
         strcpy(client_host, inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str)));
         client_port = ntohs(client_addr.sin_port);
-
-        printf("received from %s at PORT %d\n",
-                inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str)),
-                ntohs(client_addr.sin_port));
-
+#ifdef DEBUG
+        printf("received request from %s:%d and client socket is %d\n",
+               inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str)),
+               ntohs(client_addr.sin_port), client_socket);
+#endif
         if (fork() == 0)
         {
-            close(server_socket);
-            handle_client(client_socket, client_addr);
+            Close(server_socket);
+            handle_client(client_addr);
             exit(0);
         }
+
         Close(client_socket);
     }
 }
@@ -225,13 +180,15 @@ void server_deal()
 void sigchld_handler(int signal)
 {
     while (waitpid(-1, NULL, WNOHANG) > 0)
-        ;
+    {
+#ifdef DEBUG
+        printf("destroy process\n");
+#endif
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    client_buffer = (char *)malloc(MAX_BUFFER);
-
     signal(SIGCHLD, sigchld_handler);
 
     server_socket = creat_server_socket(SERV_PORT);
--- Reproxy/util.c
+++ Reproxy/util.c
@@ -152,11 +152,15 @@ static ssize_t my_read(int fd, char *ptr)
         if ((read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0)
         {
             if (errno == EINTR)
+            {
                 goto again;
+            }  
             return -1;
         }
         else if (read_cnt == 0)
+        {
             return 0;
+        }
         read_ptr = read_buf;
     }
     read_cnt--;
@@ -176,7 +180,9 @@ ssize_t Readline(int fd, void *vptr, size_t maxlen)
         {
             *ptr++ = c;
             if (c == '\n')
+            {
                 break;
+            }
         }
         else if (rc == 0)
         {
@@ -184,7 +190,9 @@ ssize_t Readline(int fd, void *vptr, size_t maxlen)
             return n - 1;
         }
         else
+        {
             return -1;
+        }    
     }
     *ptr = 0;
     return n;
